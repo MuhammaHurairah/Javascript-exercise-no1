74c1c745c7f82a03db9f82a3b66a8012da4e1653
MuhammaHurairah <muhammadhurairah455@gmail.com>
2025-02-02 23:34:18 -0800

    Add files via upload

---
+++ learning/ex1.js
@@ -0,0 +1,91 @@
+// // Task 1 
+// console.log("This is ex1 .js ")
+
+// //task 2 print your name and age 
+// let firstname = "Muhammad";
+// let lastname="hurairah";
+// let age = 20
+
+
+
+// console.log("My name is ", firstname + lastname ," and i am ",age , "year  old" );
+
+// // task 3 find moduus/ percentage of two numbers
+// let x= 101;
+// let y= 7;
+// let z= x/y;
+// console.log("The answer becomes", + z);
+
+
+// // task 4  find exponentation of 32**6
+// let a = 32;
+// let b= a**6;
+// console.log(" The reslut of 32**6 is ", + b  );
+
+// Task 5 check the number less than 9 digits
+
+// fro phone 1
+// var phone1 = 234567891;
+// var phone2 = 2344566789864;
+// var phone3 = 123456789;
+
+// // For phone 1
+// if (phone1.toString().length === 9) {
+//     console.log("The number is valid " + phone1);
+// } else {
+//     console.log("The number is invalid " + phone1);
+// }
+
+// // For phone 2
+// if (phone2.toString().length === 9) {
+//     console.log("The number is valid " + phone2);
+// } else {
+//     console.log("The number is invalid " + phone2);
+// }
+
+// // For phone 3
+// if (phone3.toString().length === 9) {
+//     console.log("The number is valid " + phone3);
+// } else {
+//     console.log("The number is invalid " + phone3);
+// }
+
+
+// task no 6 Valid and invalid naming convention 
+// valid    = var NAME;
+ //valid    = var $num1;
+ // invalid = var type of;
+// invaldi var =first-name;
+// valid var  =attempt_2;
+// invalid var= 2ndAttempt;
+// invalid  var = full name
+
+
+
+// task 7 check the data type of following element 
+
+
+// var quantity= "25";
+// var number = 6;
+// var pressure;
+// var temperature=null;
+
+
+// console.log(quantity += quantity);
+// console.log((7+5)/ number + 2)
+// console.log(pressure)
+// console.log(temperature)
+// console.log( typeof pressure)
+// console.log(typeof temperature)
+
+
+
+
+
+
+
+
+
+
+
+
